8429f78814e4763d45fc71a138146423992a4c4f
SS66260 <SS66260@host.docker.internal>
2022-05-16 19:02:46 +0530

    Adding Commits into Jobseeker and UserManagement

--- JSS/REST-API/JSS-JobSeeker/src/main/java/com/jss/jobseeker/JssJobSeekerApplication.java
+++ JSS/REST-API/JSS-JobSeeker/src/main/java/com/jss/jobseeker/JssJobSeekerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JssJobSeekerApplication {
 
+	//Hello World
 	public static void main(String[] args) {
 		SpringApplication.run(JssJobSeekerApplication.class, args);
 	}
