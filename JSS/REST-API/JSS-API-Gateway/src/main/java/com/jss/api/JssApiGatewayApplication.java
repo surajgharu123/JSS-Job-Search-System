92646e054c50038bd25fdcf83dea547442e27380
Samuel Lakra <samuel.lakra@zensar.com>
2022-05-16 19:39:43 +0530

    hello commited

--- JSS/REST-API/JSS-API-Gateway/src/main/java/com/jss/api/JssApiGatewayApplication.java
+++ JSS/REST-API/JSS-API-Gateway/src/main/java/com/jss/api/JssApiGatewayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class JssApiGatewayApplication {
 
-//hello
+//hello bhaiyo aur beheno
 	public static void main(String[] args) {
 		SpringApplication.run(JssApiGatewayApplication.class, args);
 	}
