82976b426cb6234c8a370215d71fc31cf7b66beb
Prashant Dhoke <prashant.dhoke@zensar.com>
2022-05-16 19:26:18 +0530

    Added new Commit

--- JSS/REST-API/JSS-JobSeeker/src/main/java/com/jss/jobseeker/JssJobSeekerApplication.java
+++ JSS/REST-API/JSS-JobSeeker/src/main/java/com/jss/jobseeker/JssJobSeekerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JssJobSeekerApplication {
 
-	//Hello World
+	//Hello World Hello
 	public static void main(String[] args) {
 		SpringApplication.run(JssJobSeekerApplication.class, args);
 	}
