582869eae3e5f1efc034cd6d7e46902e3c794006
shubham <shubham@gmail.com>
2022-05-17 13:44:35 +0530

    first commit

---
+++ JSS-Employee/src/main/java/com/jss/employee/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.jss.employee.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+import com.jss.employee.service.JobSeekerService;
+import com.jss.employee.dto.*;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/Employee")
+@CrossOrigin(origins = "*")
+public class EmployeeController {
+	
+	@Autowired
+	JobSeekerService jobSeekerService;
+	
+	
+	@GetMapping(value = "/search/filtercriteria", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Getting a List of JobSeeker by FilterCriteria", notes = "This Rest API will return List of JobSeeker by Filter Criteria")
+	public ResponseEntity<List<JobSeeker>> searchAdvertisesByFilterCriteria(
+			@RequestParam(name = "searchText", required = false) String skillSet)
+			 {
+		return new ResponseEntity<List<JobSeeker>>(jobSeekerService.filterAdvertise(skillSet), HttpStatus.OK);
+	}
+	
+	
+
+}
---
+++ JSS-Employee/src/main/java/com/jss/employee/service/JobSeekerService.java
@@ -0,0 +1,11 @@
+package com.jss.employee.service;
+
+import java.util.List;
+
+import com.jss.employee.dto.JobSeeker;
+
+public interface JobSeekerService {
+	
+	public List<JobSeeker> filterAdvertise(String skillSet);
+
+}
