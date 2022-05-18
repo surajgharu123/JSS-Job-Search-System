6540319d8fc0801dd06d979209ca2cbecdeaae85
prashantdhoke23 <99012605+prashantdhoke23@users.noreply.github.com>
2022-05-18 20:41:13 +0530

    Delete JSS-Employee directory

--- JSS-Employee/src/main/java/com/jss/employee/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jss.employee.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jss.employee.entity.EmployeeEntity;
-
-public interface EmployeeRepo extends JpaRepository<EmployeeEntity, Integer>{
-
-}
